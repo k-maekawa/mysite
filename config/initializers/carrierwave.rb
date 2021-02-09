23ee0edd345ce8623a884d8bd49e12e5980f5dce
k-maekawa <qian41119122@gmail.com>
2021-02-09 16:55:59 +0900

    update fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,12 @@
 unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: "AKIA2LH3H4GCA4M5TSAU",
       aws_secret_access_key: "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",
       region: "ap-southeast-1",
     }
-    config.fog_provider = 'fog/aws'
     config.fog_directory = "port-folio-s3"
-    config.cache_storage = :fog
   end
 end
